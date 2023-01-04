a0fa8866db45b234d9cb8409b7b41372d3ad7a36
Siti Zahra Azizah <114201606+stzahraPPLGA@users.noreply.github.com>
2023-01-04 15:48:56 +0800

    Add files via upload

---
+++ Untitled-1.php
@@ -0,0 +1,36 @@
+<?php
+    /* #PHP variables
+    //$x = 5; //global scope
+    function myTest() {
+        $x = 5; // local scope
+        echo "<p>variable x inside function is: $x</p>";
+    }
+    myTest();
+    echo "<p>variable x outside function is: $x</p>";
+    local scope only accessed inside function but, the global scope
+    can only accessed outside a function */
+
+    #Global keyword
+    /*$x = 5;
+    $y = 10;
+
+    function myTest(){
+        global $x, $y;
+        $y = $x + $y;
+    }
+
+    myTest(); // run function
+    echo $y; // outputs 15 */
+
+    #PHP Integer
+    /*$x = 5985;
+    var_dump($x);*/
+
+    #PHP Float
+    /*$x = 10.365;
+    var_dump($x);*/
+    
+    #PHP arry
+    /*$cars = array("Volvo", "BWM", "Toyota");
+    var_dump($cars);*/
+?>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+    #PHP Syntax
+    $color = "red";
+    echo "my car is " . $color . "<br>";
+    echo "my house is " . $color . "<br>";
+
+    #PHP Comment
+    //single-line comment
+    #comment
+
+    /*this is multiple comment
+    the comment can be in the top
+    or down*/
+
+    //we can comment in the code line
+    $z = 5 /* + 15 */ + 5;
+    echo $z;
+    echo "<br><br>";
+
+    #PHP variables
+    $txt = "W3School";
+    $z = "5";
+    $y = "10.5";
+
+    echo $txt;
+    echo "<br>";
+    echo $z;
+    echo "<br>";
+    echo $y;
+    echo "<br>";
+    echo "I Love $txt! <br>" ;
+    echo "I Love " . $txt . "!  <br>";
+    
+    $a = "5";
+    $b = "8";
+    echo $a + $b;
+    echo "<br>";
+?>
